4a40e2c23d964cb103ef6ac3585947c65235b336
FreedomPeace <18658251543@163.com>
2019-08-08 14:20:30 +0800

    完善Azprouter这个类

---
+++ arouter_api/src/main/java/com/example/arouter_api/exception/InitException.java
@@ -0,0 +1,11 @@
+package com.example.arouter_api.exception;
+
+/**
+ * 初始化相关异常
+ */
+public class InitException extends RuntimeException {
+
+    public InitException(String message) {
+        super(message);
+    }
+}
--- arouter_api/src/main/java/com/example/arouter_api/launcher/AzpRouter.java
+++ arouter_api/src/main/java/com/example/arouter_api/launcher/AzpRouter.java
@@ -1,21 +1,61 @@
 package com.example.arouter_api.launcher;
 
+import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
+
+import com.example.arouter_api.exception.InitException;
+import com.example.arouter_api.template.ILogger;
+import com.example.arouter_api.template.IRouteGroup;
+import com.example.arouter_api.util.ClassUtils;
+import com.example.arouter_api.util.DefaultLogger;
 
 import java.util.HashMap;
+import java.util.List;
+
+import static com.example.arouter_api.util.Consts.ROUTE_ROOT_PAKCAGE;
+import static com.example.arouter_api.util.Consts.TAG;
+
 
 public class AzpRouter {
     //todo 把这个类做成单列
     private  static AzpRouter router = new AzpRouter();
-    private static Context context;
-    private HashMap<String,String> activitys = new HashMap<>();
+    private static Context mContext;
+    private static HashMap<String,String> activitys = new HashMap<>();
+    private static boolean hasInit = false;
+    private static ILogger logger ;
 
-    public static AzpRouter getInstance(Context context) {
-        AzpRouter.context = context;
+    public static AzpRouter getInstance() {
         return router;
+    }
+
+    private static void init(Application context) {
+        // These class was generate by arouter-compiler.
+        mContext = context;
+        logger = new DefaultLogger(TAG);
+        try {
+            List<String> classFileNames = ClassUtils.getFileNameByPackageName(context, ROUTE_ROOT_PAKCAGE);
+            for (String classFileName : classFileNames) {
+                Class<?> aClass = Class.forName(classFileName);
+                IRouteGroup iRouteGroup = (IRouteGroup) aClass.newInstance();
+                iRouteGroup.putActivitys(activitys);
+            }
+            hasInit = true;
+        } catch (Exception e) {
+            logger.error(ILogger.defaultTag,e.getMessage());
+        }
     }
 
     public void build(String path) {
 //        Intent intent = new Intent(context, LoginActivity.class);
+        if (!hasInit) {
+            throw new InitException("使用该方法前，必须调用init 方法");
+        }
+        try {
+            Intent intent = new Intent(mContext, Class.forName(activitys.get(path)));
+        } catch (ClassNotFoundException e) {
+            logger.error(ILogger.defaultTag,e.getMessage());
+        }
     }
+
 }
