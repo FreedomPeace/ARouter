3ad3badb1510fa676f004d8eb25a166ad6909d57
FreedomPeace <18658251543@163.com>
2021-10-25 19:50:45 +0800

    更新环境到最新

--- app/src/main/java/com/example/arouter/main/Main2Activity.java
+++ app/src/main/java/com/example/arouter/main/Main2Activity.java
@@ -1,8 +1,9 @@
 package com.example.arouter.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.arouter_annotation.Route;
 
--- app/src/main/java/com/example/arouter/test/Test1Activity.java
+++ app/src/main/java/com/example/arouter/test/Test1Activity.java
@@ -1,7 +1,8 @@
 package com.example.arouter.test;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.arouter.R;
 import com.example.arouter_annotation.Route;
